6c6d08d83baa5c489107323299b2b119e320433e
BENULL <327137362@qq.com>
2021-08-22 15:24:23 +0800

    add springboot dependencies

---
+++ src/main/scala/top/benull/hitrecsys/movie/HitRecSysMovie.scala
@@ -0,0 +1,17 @@
+package top.benull.hitrecsys.movie
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * descriptions:
+ *
+ * author: benull
+ * date: 2021 - 08 - 16 14:48
+ */
+@SpringBootApplication
+class HitRecSysMovieApplication
+
+object HitRecSysMovie extends App {
+  SpringApplication.run(classOf[HitRecSysMovieApplication])
+}
