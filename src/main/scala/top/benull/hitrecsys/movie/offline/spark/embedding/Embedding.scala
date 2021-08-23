3c57e8c2197661103c871b6b53e48193c8a04215
BENULL <327137362@qq.com>
2021-08-23 15:57:47 +0800

    add Embedding

---
+++ src/main/scala/top/benull/hitrecsys/movie/offline/spark/embedding/Embedding.scala
@@ -0,0 +1,184 @@
+package top.benull.hitrecsys.movie.offline.spark.embedding
+
+import java.io.{BufferedWriter, File, FileWriter}
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.ml.feature.BucketedRandomProjectionLSH
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.{array_join, col, collect_list, struct, udf}
+import redis.clients.jedis.Jedis
+import redis.clients.jedis.params.SetParams
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * descriptions:
+ *
+ * author: benull
+ * date: 2021 - 08 - 23 14:12
+ */
+object Embedding {
+
+  val redisEndpoint = "localhost"
+  val redisPort = 6379
+
+  def processItemSequence(sparkSession: SparkSession, rawSampleDataPath: String): RDD[Seq[String]] = {
+    //path of rating data
+    val ratingsResourcesPath = this.getClass.getResource(rawSampleDataPath)
+    val ratingSamples = sparkSession.read.format("csv").option("header", "true").load(ratingsResourcesPath.getPath)
+
+    //sort by timestamp udf
+    val sortUdf: UserDefinedFunction = udf((rows: Seq[Row]) => {
+      rows.map { case Row(movieId: String, timestamp: String) => (movieId, timestamp) }
+        .sortBy { case (_, timestamp) => timestamp }
+        .map { case (movieId, _) => movieId }
+    })
+
+    ratingSamples.printSchema()
+
+    //process rating data then generate rating movie sequence data
+    var userSeq = ratingSamples
+      .where(col("rating") >= 3.5)
+      .groupBy("userId")
+      .agg(sortUdf(collect_list(struct("movieId", "timestamp"))) as "movieIds")
+      .withColumn("movieIdStr", array_join(col("movieIds"), " "))
+
+    userSeq.select("userId", "movieIdStr").show(10, truncate = false)
+    userSeq.select("movieIdStr").rdd.map(r => r.getAs[String]("movieIdStr").split(" ").toSeq)
+  }
+
+  def trainItem2vec(sparkSession: SparkSession, samples: RDD[Seq[String]], embLength: Int, embOutputFilename: String,
+                    saveToRedis: Boolean, redisKeyPrefix: String): Word2VecModel = {
+    val word2vec = new Word2Vec()
+      .setVectorSize(embLength)
+      .setWindowSize(5)
+      .setNumIterations(10)
+
+    val model = word2vec.fit(samples)
+
+    val synonyms = model.findSynonyms("158", 20)
+    for ((synonym, cosineSimilarity) <- synonyms) {
+      println(s"$synonym $cosineSimilarity")
+    }
+
+    val embFolderPath = this.getClass.getResource("/webroot/modeldata/")
+    val file = new File(embFolderPath.getPath + embOutputFilename)
+    val bw = new BufferedWriter(new FileWriter(file))
+    for (movieId <- model.getVectors.keys) {
+      bw.write(movieId + ":" + model.getVectors(movieId).mkString(" ") + "\n")
+    }
+    bw.close()
+
+    if (saveToRedis) {
+      val redisClient = new Jedis(redisEndpoint, redisPort)
+      val params = SetParams.setParams()
+      //set ttl to 24hs
+      params.ex(60 * 60 * 24)
+      for (movieId <- model.getVectors.keys) {
+        redisClient.set(redisKeyPrefix + ":" + movieId, model.getVectors(movieId).mkString(" "), params)
+      }
+      redisClient.close()
+    }
+
+    embeddingLSH(sparkSession, model.getVectors)
+    model
+  }
+
+  def generateUserEmb(sparkSession: SparkSession, rawSampleDataPath: String, word2VecModel: Word2VecModel,
+                      embLength: Int, embOutputFilename: String, saveToRedis: Boolean, redisKeyPrefix: String): Unit = {
+    val ratingsResourcesPath = this.getClass.getResource(rawSampleDataPath)
+    val ratingSamples = sparkSession.read.format("csv").option("header", "true").load(ratingsResourcesPath.getPath)
+    ratingSamples.show(10, false)
+
+    val userEmbeddings = new ArrayBuffer[(String, Array[Float])]()
+
+    ratingSamples.collect().groupBy(_.getAs[String]("userId"))
+      .foreach(user => {
+        val userId = user._1
+        var userEmb = new Array[Float](embLength)
+
+        var movieCount = 0
+        userEmb = user._2.foldRight[Array[Float]](userEmb)((row, newEmb) => {
+          val movieId = row.getAs[String]("movieId")
+          val movieEmb = word2VecModel.getVectors.get(movieId)
+          movieCount += 1
+          if (movieEmb.isDefined) {
+            newEmb.zip(movieEmb.get).map { case (x, y) => x + y }
+          } else {
+            newEmb
+          }
+        }).map((x: Float) => x / movieCount)
+        userEmbeddings.append((userId, userEmb))
+      })
+
+    val embFolderPath = this.getClass.getResource("/webroot/modeldata/")
+    val file = new File(embFolderPath.getPath + embOutputFilename)
+    val bw = new BufferedWriter(new FileWriter(file))
+
+    for (userEmb <- userEmbeddings) {
+      bw.write(userEmb._1 + ":" + userEmb._2.mkString(" ") + "\n")
+    }
+    bw.close()
+
+    if (saveToRedis) {
+      val redisClient = new Jedis(redisEndpoint, redisPort)
+      val params = SetParams.setParams()
+      //set ttl to 24hs
+      params.ex(60 * 60 * 24)
+
+      for (userEmb <- userEmbeddings) {
+        redisClient.set(redisKeyPrefix + ":" + userEmb._1, userEmb._2.mkString(" "), params)
+      }
+      redisClient.close()
+    }
+  }
+
+  def embeddingLSH(spark: SparkSession, movieEmbMap: Map[String, Array[Float]]): Unit = {
+    val movieEmbSeq = movieEmbMap.toSeq.map(item => (item._1, Vectors.dense(item._2.map(f => f.toDouble))))
+    val movieEmbDF = spark.createDataFrame(movieEmbSeq).toDF("movieId", "emb")
+
+    //LSH bucket model
+    val bucketProjectionLSH = new BucketedRandomProjectionLSH()
+      .setBucketLength(0.1)
+      .setNumHashTables(3)
+      .setInputCol("emb")
+      .setOutputCol("bucketId")
+
+    val bucketModel = bucketProjectionLSH.fit(movieEmbDF)
+    val embBucketResult = bucketModel.transform(movieEmbDF)
+
+    println("movieId, emb, bucketId schema:")
+    embBucketResult.printSchema()
+    println("movieId, emb, bucketId data result:")
+    embBucketResult.show(10, truncate = false)
+
+    println("Approximately searching for 5 nearest neighbors of the sample embedding:")
+    val sampleEmb = Vectors.dense(0.795, 0.583, 1.120, 0.850, 0.174, -0.839, -0.0633, 0.249, 0.673, -0.237)
+    bucketModel.approxNearestNeighbors(movieEmbDF, sampleEmb, 5).show(truncate = false)
+  }
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val conf = new SparkConf()
+      .setMaster("local")
+      .setAppName("ctrModel")
+      .set("spark.submit.deployMode", "client")
+
+    val spark = SparkSession.builder.config(conf).getOrCreate()
+
+    val rawSampleDataPath = "/webroot/sampledata/ratings.csv"
+    val embLength = 10
+
+    val samples = processItemSequence(spark, rawSampleDataPath)
+    val model = trainItem2vec(spark, samples, embLength, "item2vecEmb.csv", saveToRedis = false, "i2vEmb")
+
+    //    generateUserEmb(spark, rawSampleDataPath, model, embLength, "userEmb.csv", saveToRedis = false, "uEmb")
+
+  }
+}
